1a8e3c515f467ad96c51c7bc7e55974bf8d6478e
daekyounglee <102590177+daekyounglee@users.noreply.github.com>
2023-03-10 14:44:05 +0900

    Update HessianBlocks.cpp
    if this code doesn't work, comment out the code 105

--- src/FullSystem/HessianBlocks.cpp
+++ src/FullSystem/HessianBlocks.cpp
@@ -102,7 +102,7 @@ void FrameHessian::setStateZero(const Vec10 &state_zero)
 
 	nullspaces_affine.setZero();
 	nullspaces_affine.topLeftCorner<2,1>()  = Vec2(1,0);
-	assert(ab_exposure > 0);
+	assert(ab_exposure >= 0); // or comment out to get timestamp  
 	nullspaces_affine.topRightCorner<2,1>() = Vec2(0, expf(aff_g2l_0().a)*ab_exposure);
 };
 
